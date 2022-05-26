d6071f2019d4878f0308b194a124edf6c898f958
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-26 02:22:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+let largo: number = Number(
+  prompt(`Ingrese la cantida de posiciones del arreglo:`)
+);
+let arreglo1: number[] = new Array(largo);
+let arreglo2: number[] = new Array(largo);
+let productoArreglo: number = 0;
+
+function productoEscalar(vectorA: number[], vectorB: number[]): number {
+  let productoEsc: number = 0;
+  let prod: number = 0;
+  for (let i: number = 0; i < vectorA.length; i++) {
+    prod = vectorA[i] * vectorB[i];
+    productoEsc = productoEsc + prod;
+  }
+  return productoEsc;
+}
+
+for (let i: number = 0; i <= largo - 1; i++) {
+  arreglo1[i] = Number(
+    prompt(`ingrese valor para posicion ${i + 1} del Primer Arreglo`)
+  );
+  arreglo2[i] = Number(
+    prompt(`ingrese valor para posicion ${i + 1} del segundo Arreglo`)
+  );
+}
+
+productoArreglo = productoEscalar(arreglo1, arreglo2);
+
+console.log(`El producto Escalar es: ${productoArreglo}`);
